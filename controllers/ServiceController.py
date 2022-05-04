9208d755c7c6fa43074fc6054b0ba83b7829176a
Nick <lis@chaikovskie.com>
2022-05-04 22:16:26 +0300

    Добавил CRUD для сервисов

--- controllers/ServiceController.py
+++ controllers/ServiceController.py
@@ -1,4 +1,15 @@
+from typing_extensions import TypedDict
 from db.Database import Database
+import requests
+
+class ServiceCreateDto(TypedDict):
+  title: str
+  url: str
+
+class ServiceUpdateDto(TypedDict):
+  id: int
+  title: str
+  url: str
 
 class ServiceController:
   def __init__(self, DB: Database):
@@ -11,7 +22,41 @@ class ServiceController:
     ''')
 
   def get_by_id(self, id: int):
-    return self.DB.query(f'''
-      SELECT * FROM services WHERE id = {id}
+    service = self.DB.query(f'''
+      SELECT * FROM services 
       INNER JOIN users ON users.id = services.user_id
+      WHERE services.id = {id}
+    ''')
+
+    if not service:
+      return None
+
+    return service[0]
+ 
+  def create(self, data: ServiceCreateDto):
+    status = None
+    try:
+      url_req = requests.get(data['url'])
+      status = url_req.status_code
+    except:
+      status = 500
+
+    return self.DB.query(f'''
+      INSERT INTO 
+        services (title, url, status, user_id)
+      VALUES 
+        ('{data['title']}', '{data['url']}', {status}, 1)
+    ''')
+  
+  def update(self, data: ServiceUpdateDto):
+    return self.DB.query(f'''
+      UPDATE services
+      SET title = "{data['title']}", url = "{data['url']}"
+      WHERE id = {data['id']}
+    ''')
+  
+  def delete(self, id: int):
+    return self.DB.query(f'''
+      DELETE FROM services
+      WHERE id = {id}
     ''')
--- main.py
+++ main.py
@@ -1,21 +1,74 @@
 from db.Database import Database
 from controllers.AnnouncementController import AnnouncementController
-from controllers.ServiceController import ServiceController
+from controllers.ServiceController import ServiceController, ServiceCreateDto
 from controllers.UserController import UserController
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for, flash, redirect
 
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'q7s3FTmXKGuRHvaFHcoreZHDZ4mpEDpu'
 
 @app.route('/')
 def index():
   db = Database()
-
   controller = ServiceController(db)
-
   services = controller.get_all()
 
   return render_template('pages/index.html', services=services)
+
+@app.route('/create', methods=('GET', 'POST'))
+def create_service():
+  if request.method == 'POST':
+    title = request.form['title']
+    url = request.form['url']
+
+    if not title:
+      flash('Пожалуйста, укажите название сервиса', 'danger')
+    elif not url:
+      flash('Пожалуйста, укажите URL сервиса', 'danger')
+    else:
+      db = Database()
+      controller = ServiceController(db)
+      controller.create({ 'title': title, 'url': url })
+      
+      return redirect(url_for('index')) 
+
+  return render_template('pages/create.html')
+
+@app.route('/<int:id>/edit', methods=('GET', 'POST'))
+def edit_service(id: int):
+  db = Database()
+  controller = ServiceController(db)
+
+  service = controller.get_by_id(id)
+
+  if request.method == 'POST':
+    title = request.form['title']
+    url = request.form['url']
+
+    if not title:
+      flash('Пожалуйста, укажите название сервиса', 'danger')
+    elif not url:
+      flash('Пожалуйста, укажите URL сервиса', 'danger')
+    else:
+      controller.update({ 'id': id, 'title': title, 'url': url })
+      
+      return redirect(url_for('index')) 
+
+  return render_template('pages/edit.html', service=service)
+
+@app.route('/<int:id>/delete', methods=('POST',))
+def delete(id):
+  db = Database()
+  controller = ServiceController(db)
+
+  service = controller.get_by_id(id)
+  controller.delete(id)
+
+  flash(f'Сервис {service["title"]} был успешно удалён', 'info')
+
+  return redirect(url_for('index'))
 
 if __name__ == '__main__':
   app.run(debug=True)
