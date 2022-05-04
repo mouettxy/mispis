b159452ffff2c3b69e3145a6bf982ad9f3cd2556
Nick <lis@chaikovskie.com>
2022-05-04 12:06:13 +0300

    Добавил шаблоны и главную

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
-
-
-
-
-
 from db.Database import Database
 from controllers.AnnouncementController import AnnouncementController
 from controllers.ServiceController import ServiceController
 from controllers.UserController import UserController
 
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+ return render_template('pages/index.html')
 
 if __name__ == '__main__':
   db = Database()
@@ -19,3 +21,5 @@ if __name__ == '__main__':
   print(user.get_all())
   print(service.get_all())
   print(announcement.get_all())
+
+  app.run(debug=True)
